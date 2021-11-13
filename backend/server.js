bb6f383b37dde17a792ae2322bcb66e464639b54
NooraAli20 <70133983+NooraAli20@users.noreply.github.com>
2021-11-13 07:13:19 +0100

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import HamsterRouter from "./routes/hamster-routes.js";
 import MatchesRouter from "./routes/matches-routes.js";
 import AllRouter from "./routes/all-routes.js";
 import dotenv from 'dotenv';
+import path from 'path';
 
 dotenv.config();
 
@@ -11,6 +12,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 app.use((req, res, next) => {
     console.log(`${req.method}  ${req.url} `, req.body)
     next()
@@ -20,7 +22,7 @@ app.use('/hamsters', HamsterRouter);
 app.use('/matches', MatchesRouter);
 app.use('/', AllRouter)
 
-const PORT = parseInt(process.env.PORT)
+const PORT = parseInt(process.env.PORT) || 3001
 app.listen(PORT, () => {
     console.log('App is listening on port '+ PORT);
 });
