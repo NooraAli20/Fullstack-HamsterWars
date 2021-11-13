93a38869b3c219e1b4708fb69d5cfbef7f9c157e
NooraAli20 <70133983+NooraAli20@users.noreply.github.com>
2021-11-13 01:22:29 +0100

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import HamsterRouter from "./routes/hamster-routes.js";
 import MatchesRouter from "./routes/matches-routes.js";
 import AllRouter from "./routes/all-routes.js";
 import dotenv from 'dotenv';
+import path from 'path';
 
 dotenv.config();
 
@@ -11,6 +12,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 app.use((req, res, next) => {
     console.log(`${req.method}  ${req.url} `, req.body)
     next()
